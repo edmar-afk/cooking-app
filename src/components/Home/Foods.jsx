9c239df4e7ef923140c08a0f8aaec61f14b0e1b7
edmar-afk <jaywrsnp6@gmail.com>
2025-08-08 12:43:01 +0800

    new

--- src/components/Home/Foods.jsx
+++ src/components/Home/Foods.jsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from "react";
-import BlenderIcon from "@mui/icons-material/Blender";
 import GroupIcon from "@mui/icons-material/Group";
 import api from "../../assets/api";
 import { Link } from "react-router-dom";
@@ -17,18 +16,26 @@ function Foods() {
 	return (
 		<>
 			{foods.map((food) => (
-				<Link to={`/recipe/${food.id}`}
+				<Link
+					to={`/recipe/${food.id}`}
 					key={food.id}
 					className="relative isolate flex flex-col justify-end overflow-hidden rounded-2xl px-4 pb-4 pt-40 w-[250px] h-[250px] mx-auto mt-4">
+					
 					<img
 						src={food.image}
 						alt={food.name}
 						className="absolute inset-0 h-full w-full object-cover"
 					/>
-					<div className="absolute inset-0 bg-gradient-to-t from-gray-950 via-gray-950/20"></div>
-					<h3 className="z-10 mt-3 text-xl font-bold text-white">{food.name}</h3>
-					<p className="z-10 text-white text-xs mr-4 flex flex-row items-center">{food.title}</p>
-					<div className="z-10 flex flex-row mt-3 text-xs leading-6 text-gray-300">
+
+					<div className="absolute inset-0 bg-gradient-to-t custom-food-from-gray-950 custom-food-via-gray-950-20"></div>
+
+					<h3 className="z-10 mt-3 text-xl font-bold custom-food-text-white">{food.name}</h3>
+
+					<p className="z-10 custom-food-text-white text-xs mr-4 flex flex-row items-center">
+						{food.title}
+					</p>
+
+					<div className="z-10 flex flex-row mt-3 text-xs leading-6 custom-food-text-gray-300">
 						<span className="flex flex-row items-center">
 							<GroupIcon fontSize="small" className="mr-2" />
 							Good for {food.serve} Persons
