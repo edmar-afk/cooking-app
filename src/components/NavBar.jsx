b820899b575cfe0f9327485616e7b92a28a5c7d0
edmar-afk <jaywrsnp6@gmail.com>
2025-08-08 11:27:31 +0800

    new

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,43 +1,73 @@
-import { NavLink } from "react-router-dom";import HomeIcon from "@mui/icons-material/Home";
+import { NavLink, useLocation } from "react-router-dom";
+import HomeIcon from "@mui/icons-material/Home";
 import Person2Icon from "@mui/icons-material/Person2";
 import FavoriteIcon from "@mui/icons-material/Favorite";
+
 function NavBar() {
-	return (
-		<div className="fixed bottom-0 left-0 z-50 w-full h-fit px-2 bg-white border-t border-gray-200">
-			<div className="flex items-center justify-evenly">
-				<NavLink
-					to="/home"
-					end
-					className={({ isActive }) => `p-2.5 flex flex-col items-center ${isActive ? "text-orange-600" : ""}`}>
-					<HomeIcon style={{ color: '#000' }}
-						className=" bg-orange-500 hover:bg-orange-200 hover:text-orange-800 duration-300 p-2 rounded-full"
-						fontSize="large"
-					/>
-					<p className={`text-xs ${location.pathname === "/" ? "underline font-bold" : ""}`}>Homes</p>
-				</NavLink>
+  const location = useLocation();
 
-				<NavLink
-					to="/favorites"
-					className={({ isActive }) => `p-2.5 flex flex-col items-center ${isActive ? "text-orange-600" : ""}`}>
-					<FavoriteIcon
-						className="text-white bg-orange-500 hover:bg-orange-200 hover:text-orange-800 duration-300 p-2 rounded-full"
-						fontSize="large"
-					/>
-					<p className={`text-xs ${location.pathname === "/favorites" ? "underline font-bold" : ""}`}>Favorites</p>
-				</NavLink>
+  return (
+    <div className="fixed bottom-0 left-0 z-50 w-full h-fit px-2 bg-white border-t border-gray-200">
+      <div className="flex items-center justify-evenly">
+        <NavLink
+          to="/home"
+          end
+          className={({ isActive }) =>
+            `p-2.5 flex flex-col items-center ${
+              isActive ? "nav-text active" : "nav-text"
+            }`
+          }
+        >
+          <HomeIcon className="nav-icon" fontSize="large" />
+          <p
+            className={
+              location.pathname === "/" ? "nav-text active" : "nav-text"
+            }
+          >
+            Homes
+          </p>
+        </NavLink>
 
-				<NavLink
-					to="/profile"
-					className={({ isActive }) => `p-2.5 flex flex-col items-center ${isActive ? "text-orange-600" : ""}`}>
-					<Person2Icon
-						className="text-white bg-orange-500 hover:bg-orange-200 hover:text-orange-800 duration-300 p-2 rounded-full"
-						fontSize="large"
-					/>
-					<p className={`text-xs ${location.pathname === "/profile" ? "underline font-bold" : ""}`}>Profile</p>
-				</NavLink>
-			</div>
-		</div>
-	);
+        <NavLink
+          to="/favorites"
+          className={({ isActive }) =>
+            `p-2.5 flex flex-col items-center ${
+              isActive ? "nav-text active" : "nav-text"
+            }`
+          }
+        >
+          <FavoriteIcon className="nav-icon" fontSize="large" />
+          <p
+            className={
+              location.pathname === "/favorites"
+                ? "nav-text active"
+                : "nav-text"
+            }
+          >
+            Favorites
+          </p>
+        </NavLink>
+
+        <NavLink
+          to="/profile"
+          className={({ isActive }) =>
+            `p-2.5 flex flex-col items-center ${
+              isActive ? "nav-text active" : "nav-text"
+            }`
+          }
+        >
+          <Person2Icon className="nav-icon" fontSize="large" />
+          <p
+            className={
+              location.pathname === "/profile" ? "nav-text active" : "nav-text"
+            }
+          >
+            Profile
+          </p>
+        </NavLink>
+      </div>
+    </div>
+  );
 }
 
 export default NavBar;
